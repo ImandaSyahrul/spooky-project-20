aea5d5fdbbd3e66eae892e8f8d0f78604258f752
ImandaSyahrul <ImandaSyahrul@github.com>
2020-09-10 04:23:46 +0700

    Add 10 portraits for Dina

---
+++ Assets/Scripts/NarrativeController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+public class NarrativeController : MonoBehaviour
+{
+    [Tooltip("Global Volume to blur environment")]
+    public Volume globalVolume;
+    [Tooltip("GameObject that contain cinematic panel")]
+    public GameObject cinematicPanel;
+
+    public void SetNarrativeVolume()
+    {
+        globalVolume.weight = 0.9f;
+    }
+
+    public void SetEnvironmentVolume()
+    {
+        globalVolume.weight = 0f;
+    }
+
+    public void TurnOnCinematic()
+    {
+        cinematicPanel.SetActive(true);
+    }
+
+    public void TurnOffCinematic()
+    {
+        cinematicPanel.SetActive(false);
+    }
+
+    public void FadeIn()
+    {
+
+    }
+}
