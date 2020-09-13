e0d918b3f598acb5eabb8337ca272772c373a224
ImandaSyahrul <ImandaSyahrul@github.com>
2020-09-13 16:55:44 +0700

    Modify log button on side menu

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -15,4 +15,16 @@ public class CameraController : MonoBehaviour
     {
         
     }
+
+    public void GoToWindow()
+    {
+        transform.position = new Vector3(218.668f, 7.584f, -36.413f);
+        transform.rotation = new Quaternion(0f,10f,0f,270f);
+    }
+
+    public void GoToFront()
+    {
+        transform.position = new Vector3(220.966f, 7.348f, -35.874f);
+        transform.rotation = new Quaternion(0f, 10f, 0f, 0f);
+    }
 }
