ffae48a6b188576eb30108d0d6c28bc276b12a05
ImandaSyahrul <ImandaSyahrul@github.com>
2020-09-20 18:15:40 +0700

    Add Menu Dialog GUI

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,19 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class CameraController : MonoBehaviour
 {
+    public Volume volume; 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ToggleBlur()
+    {
+        volume.weight = 1;
     }
 
     public void GoToWindow()
