6425ec745569a01b00f7a4538b851e1a06bef4fc
ardhanfadh <dandut88@gmail.com>
2020-09-24 21:27:42 +0700

    Updaed_MainMenu_UI

--- Assets/Scripts/MainMenuTransition.cs
+++ Assets/Scripts/MainMenuTransition.cs
@@ -36,7 +36,6 @@ public class MainMenuTransition : MonoBehaviour
     {
             Button_Start.Panah_Nyala();
             Button_Setting.Panah_Mati();
-            Button_Continue.Panah_Mati();
             Button_Quit.Panah_Mati();
     }
 
@@ -48,28 +47,14 @@ public class MainMenuTransition : MonoBehaviour
     public void HoverButton_Setting()
     {
             Button_Start.Panah_Mati();
-            Button_Continue.Panah_Mati();
             Button_Setting.Panah_Nyala();
             Button_Quit.Panah_Mati();
     }
 
-    public void ClickButton_Continue()
-    {
-            In_AnimationLoad();
-    }
-
-    public void HoverButton_Continue()
-    {
-            Button_Start.Panah_Mati();
-            Button_Continue.Panah_Nyala();
-            Button_Setting.Panah_Mati();
-            Button_Quit.Panah_Mati();
-    }
 
     public void HoverButton_Quit()
     {
             Button_Start.Panah_Mati();
-            Button_Continue.Panah_Mati();
             Button_Setting.Panah_Mati();
             Button_Quit.Panah_Nyala();
     }
@@ -77,7 +62,6 @@ public class MainMenuTransition : MonoBehaviour
     public void RestartStateHover()
     {
          Button_Start.Panah_Mati();
-         Button_Continue.Panah_Mati();
          Button_Setting.Panah_Mati();
          Button_Quit.Panah_Mati();
     }
@@ -94,17 +78,6 @@ public class MainMenuTransition : MonoBehaviour
         SettingPanel.DOPlay();
     }
 
-    public void In_AnimationLoad()
-    {
-        LoadPanel.gameObject.transform.DOLocalMoveY(0,1,true);
-        LoadPanel.DOPlay();
-    }
-
-    public void Out_AnimationLoad()
-    {
-        LoadPanel.gameObject.transform.DOLocalMoveY(867,1,true);
-        LoadPanel.DOPlay();
-    }
 
     public void In_AnimationStart()
     {
